80e59aab1ae36a7bd4dfa45d7faa7bc07a922b85
v.drachenko <only-victor@mail.ru>
2026-05-17 11:16:25 +0300

    Правка

--- src/app/src/bin/main.rs
+++ src/app/src/bin/main.rs
@@ -10,30 +10,19 @@ use core::ffi::{c_char, c_int};
 #[allow(unused_imports)]
 use {
     app_base::prelude::*,
-    app_kernel::{
-        app_cdylib_mod,
-        prelude::{App, AppModule}
-    }
+    app_kernel::prelude::*,
+    app::MOD_MAIN
 };
-
-app_cdylib_mod!("app", MOD_MAIN, app::MOD_MAIN);
-
-#[cfg(prefer_dynamic)]
-#[link(name = "app")]
-unsafe extern "C" {
-    #[allow(improper_ctypes)]
-    fn app_main(modules: &[AppModule]) -> Void;
-}
 
 #[cfg(feature = "std")]
 fn main() -> Void {
-    App::new(&[MOD_MAIN()])?.run(None).void()
+    App::new(&[MOD_MAIN])?.run(None).void()
 }
 
 #[cfg(not(feature = "std"))]
 #[unsafe(no_mangle)]
 fn main(argc: c_int, argv: *const *const c_char) -> c_int {
-    let res = unsafe { app_main(&[MOD_MAIN()], argc, argv) };
+    let res = App::new(&[MOD_MAIN], argc, argv)?.run(None).void();
 
     if let Err(e) = res {
         eprintln!("Error: {e}");
